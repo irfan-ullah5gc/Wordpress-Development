01bd1d8fff40288c56bce1ce8b74e0bbc68a0fc7
irfan-ullah5gc <irfanullah5gc@gamil.com>
2024-10-01 23:25:29 +0500

    07 folder - lecture 008 done

--- themes/fictional-university-theme/front-page.php
+++ themes/fictional-university-theme/front-page.php
@@ -20,7 +20,7 @@
           $today = date('Ymd');
             $homepageEvents = new WP_Query(array(
               'post_type' => 'event',
-              'posts_per_page' => -1,
+              'posts_per_page' => 2,
               'meta_key' => 'event_date',
               'orderby' => 'meta_value_num',
               'order' => 'ASC',
