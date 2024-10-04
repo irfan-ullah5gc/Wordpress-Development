8fd371615f0917ba5e36077e5b720e77d45aba20
irfan-ullah5gc <irfanullah5gc@gamil.com>
2024-10-04 10:19:43 +0500

    10 folder - lecture 001 done

--- themes/fictional-university-theme/page.php
+++ themes/fictional-university-theme/page.php
@@ -3,17 +3,10 @@
    while(have_posts()){
 
       the_post();
+      pageBanner(array(
+         'photo' => 'https://blog.engram.us/content/images/size/w1000/2024/01/clemence-bergougnoux-EE1B5Kvu6yg-unsplash--1-.jpg'
+      ));
       ?>
-
-      <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri( '/images/ocean.jpg' ) ?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
-          <p>Dont forget to replace me later</p>
-        </div>
-      </div>
-    </div>
 
     <div class="container container--narrow page-section">
 
